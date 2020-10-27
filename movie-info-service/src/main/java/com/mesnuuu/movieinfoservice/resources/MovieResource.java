7c776663d9cbf63ee340deb6e68cbeaef1979ed9
mesnuuu <46952459+mesnuuu@users.noreply.github.com>
2020-10-27 08:17:20 +0500

    03. Added Movie Info API

---
+++ movie-info-service/src/main/java/com/mesnuuu/movieinfoservice/resources/MovieResource.java
@@ -0,0 +1,17 @@
+package com.mesnuuu.movieinfoservice.resources;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mesnuuu.movieinfoservice.models.Movie;
+
+@RestController
+public class MovieResource {
+
+	@GetMapping("/movies/{movieId}")
+	
+	public Movie getMovies(@PathVariable("movieId") String movieId) {
+		 return new Movie("movie id", "Terminator");
+	}
+}
