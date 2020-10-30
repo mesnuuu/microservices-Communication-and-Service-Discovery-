f9d7fd7391a2919071b1944b3b0fe0e94ef553ec
mesnuuu <46952459+mesnuuu@users.noreply.github.com>
2020-10-30 21:31:19 +0500

    08. Calling Ratings Data API

--- movie-catalog-service/src/main/java/com/mesnuuu/moviecatalogservice/resources/MovieCatalogeResource.java
+++ movie-catalog-service/src/main/java/com/mesnuuu/moviecatalogservice/resources/MovieCatalogeResource.java
@@ -14,26 +14,28 @@ import org.springframework.web.client.RestTemplate;
 import com.mesnuuu.moviecatalogservice.models.CatalogItem;
 import com.mesnuuu.moviecatalogservice.models.Movie;
 import com.mesnuuu.moviecatalogservice.models.Rating;
+import com.mesnuuu.moviecatalogservice.models.UserRating;
 
 @RestController
 public class MovieCatalogeResource {
 
 	@Autowired
 	private RestTemplate restTemplate;
+	private String ratingsDataServiceUrl = "http://localhost:8083/ratingsdata/user/fee";
+	private String movieInfoServiceUrl = "http://localhost:8082/movies/";
 
 	@GetMapping("/catalog/{userId}")
 	public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
 
-		List<Rating> ratingsList = Arrays.asList(
-				new Rating("1234", 3),
-				new Rating("5678", 4)
+		UserRating userRating;
+		userRating = restTemplate.getForObject(ratingsDataServiceUrl + userId,
+				UserRating.class);
 
-		);
-
-		return ratingsList.stream().map(rating -> {
-			Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
-			return new CatalogItem(movie.getName(), "description", rating.getRating());
+		return userRating.getRatings().stream().map(rating -> {
+			Movie movie = restTemplate.getForObject(movieInfoServiceUrl + rating.getMovieId(), Movie.class);
+			return new CatalogItem(movie.getName(), "Description", rating.getRating());
 		}).collect(Collectors.toList());
+
 	}
 
 }
