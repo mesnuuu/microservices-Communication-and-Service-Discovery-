ea48951d7ccfcc50b2f9615e6b0ff62348fab77c
mesnuuu <46952459+mesnuuu@users.noreply.github.com>
2020-10-31 12:52:58 +0500

    Server Running of Java 11
    Unable to configure Eureka on Java 15.
    Successfully running on Java 11.

--- eurekaserver/src/test/java/com/example/demo/EurekaserverApplicationTests.java
+++ eurekaserver/src/test/java/com/example/demo/EurekaserverApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Eurekaserver1ApplicationTests {
+class EurekaserverApplicationTests {
 
 	@Test
 	void contextLoads() {
