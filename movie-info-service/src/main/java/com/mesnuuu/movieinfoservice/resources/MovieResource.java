8be9b28ebbf072b7d6e39b953a8f7d1211b6d599
mesnuuu <46952459+mesnuuu@users.noreply.github.com>
2020-10-31 19:13:18 +0500

    10 - Converted Services to Eureka Clients

--- movie-info-service/src/main/java/com/mesnuuu/movieinfoservice/resources/MovieResource.java
+++ movie-info-service/src/main/java/com/mesnuuu/movieinfoservice/resources/MovieResource.java
@@ -1,5 +1,6 @@
 package com.mesnuuu.movieinfoservice.resources;
 
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.mesnuuu.movieinfoservice.models.Movie;
 
 @RestController
+@EnableEurekaClient
 public class MovieResource {
 
 	@GetMapping("/movies/{movieId}")
